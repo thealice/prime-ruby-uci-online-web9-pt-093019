358e6f80e30bd3e804f242a0dec197af08f48c49
Alice Freda <alicefreda@gmail.com>
2019-10-17 17:51:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(num)
-  if num <= 1
-    return false
-  else
-  (2..Math.sqrt(num)).none? { |i| (num % i) == 0 }
-end
+    if num <= 1
+      return false
+    else
+    (2..Math.sqrt(num)).none? { |i| (num % i) == 0 }
+  end
 end
